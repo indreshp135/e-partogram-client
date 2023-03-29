f490b42294efa4249e000a33d3d9f214aa1106dc
Anirudh V S <54744932+anirudhvs@users.noreply.github.com>
2023-03-30 02:59:28 +0530

    Added requests

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -5,7 +5,12 @@ import {
   REGISTER_URL,
   LOGOUT_URL,
   USER_URL,
-  GET_ROLES_URL
+  GET_ROLES_URL,
+  ADD_PATIENT_URL,
+  LIST_PATIENTS_URL,
+  GET_PATIENT_URL,
+  ADD_MEASUREMENT_URL,
+  LIST_ONDUTY_STAFF_URL
 } from './urls';
 import { appCheck } from './firebase';
 
@@ -55,3 +60,79 @@ export const userRequest = ({
 
 // roles
 export const getRolesRequest = () => axios.get(`${GET_ROLES_URL}`, requestConfig);
+
+// add patient
+export const addPatientRequest = (token, {
+  name,
+  age,
+  parity,
+  alive,
+  edd,
+  sb,
+  nnd,
+  riskFactors,
+  contractionStartTime,
+  membraneRuptureTime,
+  height,
+  doctor,
+  nurse
+}) => axios.post(`${ADD_PATIENT_URL}`, {
+  name,
+  age,
+  parity,
+  alive,
+  edd,
+  sb,
+  nnd,
+  riskFactors,
+  contractionStartTime,
+  membraneRuptureTime,
+  height,
+  doctor,
+  nurse
+}, {
+  ...requestConfig,
+  headers: {
+    'X-Token-Firebase': token
+  }
+});
+
+// get patient list
+export const listPatientsRequest = (token) => axios.get(`${LIST_PATIENTS_URL}`, {
+  ...requestConfig,
+  headers: {
+    'X-Token-Firebase': token
+  }
+});
+
+// get patient
+export const getPatientRequest = (token, id) => axios.get(`${GET_PATIENT_URL}/${id}`, {
+  ...requestConfig,
+  headers: {
+    'X-Token-Firebase': token
+  }
+});
+
+// add measurement
+export const addMeasurementRequest = (token, {
+  patientId,
+  measurementType,
+  measurementValue
+}) => axios.post(`${ADD_MEASUREMENT_URL}`, {
+  patientId,
+  measurementType,
+  measurementValue
+}, {
+  ...requestConfig,
+  headers: {
+    'X-Token-Firebase': token
+  }
+});
+
+// list on onduty staff
+export const listOnDutyStaffRequest = (token) => axios.get(`${LIST_ONDUTY_STAFF_URL}`, {
+  ...requestConfig,
+  headers: {
+    'X-Token-Firebase': token
+  }
+});
--- src/utils/urls.js
+++ src/utils/urls.js
@@ -7,4 +7,11 @@ export const LOGIN_URL = `${BACKEND_URL}/auth/login`;
 export const REGISTER_URL = `${BACKEND_URL}/auth/register`;
 export const LOGOUT_URL = `${BACKEND_URL}/auth/logout`;
 export const USER_URL = `${BACKEND_URL}/user`;
+
 export const GET_ROLES_URL = `${BACKEND_URL}/auth/roles`;
+export const ADD_PATIENT_URL = `${BACKEND_URL}/patient/add`;
+export const LIST_PATIENTS_URL = `${BACKEND_URL}/patient/list`;
+export const GET_PATIENT_URL = `${BACKEND_URL}/patient/`;
+export const ADD_MEASUREMENT_URL = `${BACKEND_URL}/measurement/add`;
+
+export const LIST_ONDUTY_STAFF_URL = `${BACKEND_URL}/user/onduty`;
