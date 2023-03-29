7413609c0bf6787bc078105243f612ef8a5aa422
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2023-03-29 18:03:10 +0530

    Add firebase emulator config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
-import { getAuth } from 'firebase/auth';
+import { connectAuthEmulator, getAuth } from 'firebase/auth';
+import { connectDatabaseEmulator, getDatabase } from 'firebase/database';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { connectStorageEmulator, getStorage } from 'firebase/storage';
 import { appCheckDebugToken, firebaseConfig, recaptchaSiteKey } from '../config';
 
 const app = initializeApp(firebaseConfig);
@@ -16,5 +19,16 @@ const appCheck = initializeAppCheck(app, {
 });
 
 const auth = getAuth(app);
+const db = getDatabase(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+if (process.env.NODE_ENV === 'development') {
+  connectAuthEmulator(auth, 'http://localhost:9099');
+  connectDatabaseEmulator(db, 'localhost', 9009);
+  connectFirestoreEmulator(firestore, 'localhost', 8088);
+  connectStorageEmulator(storage, 'localhost', 9199);
+}
 
-export { auth, app, appCheck };
+export {
+  auth, app, appCheck, db, firestore, storage
+};
